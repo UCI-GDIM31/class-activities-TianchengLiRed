48c1a2350ad7c0a34dd5effc1d14252a0c1e636d
TianchengLiRed <tiancl28@uci.edu>
2025-10-27 17:37:54 -0700

    Update DeerW5.cs

--- In Class Activities/Assets/W5/Scripts/DeerW5.cs
+++ In Class Activities/Assets/W5/Scripts/DeerW5.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework.Internal;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -5,3 +6,14 @@ using UnityEngine.AI;
 // Hint: if you don't remember what a class is supposed to look like,
 //      maybe check out CatW5...
 // If you copied the class declaration from CatW5, you'd only need to change one thing...
+public class DeerW5: MonoBehaviour
+{
+    public Vector3 vector;
+    private void Start( Collision other)
+    {
+        Vector3.MoveTowards(other.gameObject.GetComponent<NavMesh>());
+        
+    
+    
+    }
+}
