408824153de1dec870fc08b5ffbc1d2e38190988
TianchengLiRed <tiancl28@uci.edu>
2025-11-03 19:32:52 -0800

    Update BatW6.cs

--- In Class Activities/Assets/W6/Scripts/BatW6.cs
+++ In Class Activities/Assets/W6/Scripts/BatW6.cs
@@ -1,7 +1,32 @@
+using TMPro;
 using UnityEngine;
 
 // Write the BatW6 class here.
 public class BatW6 : MonoBehaviour
 {
+    [SerializeField] private float _speed;
+    private Transform _playerPosition;
+
+    void Start()
+    {
+        enabled = false;
+    }
+    public void Chase(Transform playerPosition)
+    {
+        enabled = true;
+        _playerPosition = playerPosition;
     
+    }
+
+    public void Stop()
+    {
+        enabled = false;
+    }
+    
+    private void Update()
+    {
+        Debug.Log(_playerPosition.position);
+        transform.position = Vector3.MoveTowards(transform.position, _playerPosition.position, _speed * Time.deltaTime);
+      
+    }
 }
